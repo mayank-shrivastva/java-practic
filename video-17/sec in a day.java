8ac9a6a7927f4081574dd470a82d4b00322dde6c
mayank-shrivastva <100014914+mayank-shrivastva@users.noreply.github.com>
2023-03-25 12:00:25 +0530

    Create sec in a day.java

---
+++ video-17/sec in a day.java
@@ -0,0 +1,12 @@
+import java.util.*;
+  
+  public class Main{
+       public static void main (String[] args) {
+           Scanner sc = new Scanner(System.in);
+           int i;
+            System.out.println("enter number day");
+            int n= sc.nextInt();
+             System.out.println(24*n*60*60);
+            }
+           
+       }
