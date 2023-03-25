e217364fc3fa0b2de3a655f005c8caa9d3669a24
mayank-shrivastva <100014914+mayank-shrivastva@users.noreply.github.com>
2023-03-25 18:50:58 +0530

    Create reverse a number.java

---
+++ reverse a number.java
@@ -0,0 +1,15 @@
+    import java.util.*;
+public class Main {
+  
+     public static void main(String[] args) {
+          // TODO Auto-generated method stub
+          Scanner sc= new Scanner(System.in);
+          
+          int  n =sc.nextInt();
+          StringBuilder str2 = new StringBuilder();
+          str2.append(n);
+          str2 = str2.reverse();     // used string builder to reverse
+         System.out.println(str2);
+          }
+  
+}
